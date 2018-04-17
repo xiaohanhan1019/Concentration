5e2ab42785f9edb06829f4f06a9a7364d2cf0ddc
xiaohanhan <xiaohanhan1019@163.com>
2018-04-17 19:32:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaohanhan/concentration/TaskListActivity.java
@@ -0,0 +1,52 @@
+package com.example.xiaohanhan.concentration;
+
+import android.media.Image;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    ImageView mShowChartImageView;
+    ImageView mSettingsImageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        //replace actionbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new TaskListFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+
+        mShowChartImageView = findViewById(R.id.show_chart);
+        mShowChartImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 切换activity
+            }
+        });
+
+        mSettingsImageView = findViewById(R.id.settings);
+        mSettingsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 切换activity
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/xiaohanhan/concentration/TaskListFragment.java
@@ -0,0 +1,23 @@
+package com.example.xiaohanhan.concentration;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xiaohanhan on 2018/4/17.
+ */
+
+public class TaskListFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_task_list,container,false);
+
+        return v;
+    }
+}
