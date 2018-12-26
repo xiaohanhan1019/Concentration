11f44d2bb6509920840ca339a41a0cabfa176e07
xiaohanhan1019 <xiaohanhan1019@users.noreply.github.com>
2018-12-26 15:01:09 +0800

    db_change
    db_change

--- app/src/main/java/com/example/xiaohanhan/concentration/Util/MySQLHelper.java
+++ app/src/main/java/com/example/xiaohanhan/concentration/Util/MySQLHelper.java
@@ -25,9 +25,9 @@ import java.util.List;
 public class MySQLHelper {
 
     private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://106.2.23.8/songzihan?connectTimeout=2000";
-    private static final String user = "mysql";
-    private static final String password = "mysql";
+    private static final String url = "jdbc:mysql://10.0.2.2/concentration?connectTimeout=2000";
+    private static final String user = "root";
+    private static final String password = "1019";
 
     private static Connection getConn(){
         Connection conn = null;
